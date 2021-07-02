16fd4145df14d06e9ec772e9c8c56314142e80b6
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2021-07-02 11:16:31 +0200

    Added WebSocket GraphQL endpoint

--- src/main/kotlin/dev/marcinkiewicz/graphql/MainVerticle.kt
+++ src/main/kotlin/dev/marcinkiewicz/graphql/MainVerticle.kt
@@ -6,6 +6,8 @@ import dev.marcinkiewicz.graphql.tasks.adapters.taskdb.InMemoryTaskRepository
 import dev.marcinkiewicz.graphql.tasks.adapters.taskservice.TaskServiceImpl
 import dev.marcinkiewicz.graphql.tasks.config.graphql.GraphQlConfiguration.setupGraphQlSchemaAndWiring
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpServerOptions
+import io.vertx.ext.web.handler.graphql.ApolloWSHandler
 import io.vertx.ext.web.handler.graphql.GraphQLHandler
 import io.vertx.ext.web.handler.graphql.GraphiQLHandler
 import io.vertx.kotlin.ext.web.handler.graphql.graphiQLHandlerOptionsOf
@@ -13,14 +15,13 @@ import io.vertx.kotlin.ext.web.handler.graphql.graphiQLHandlerOptionsOf
 class MainVerticle : AbstractVerticle() {
 
   override fun start() {
-    val graphQlHandler: GraphQLHandler =
-      GraphQLHandler.create(
-        setupGraphQlSchemaAndWiring(
-          vertx,
-          TaskFacade(TaskServiceImpl(InMemoryTaskRepository())) // Dependency injection at its finest :)
-        )
-      )
+    val graphQl = setupGraphQlSchemaAndWiring(
+      vertx,
+      TaskFacade(TaskServiceImpl(InMemoryTaskRepository())) // Dependency injection at its finest :)
+    )
 
+    val graphQlHandler: GraphQLHandler = GraphQLHandler.create(graphQl)
+    val wsHandler = ApolloWSHandler.create(graphQl)
     val graphiQlHandler: GraphiQLHandler = GraphiQLHandler.create(
       graphiQLHandlerOptionsOf(
         enabled = true,
@@ -28,8 +29,8 @@ class MainVerticle : AbstractVerticle() {
       )
     )
 
-    vertx.createHttpServer()
-      .requestHandler(applicationRouter(vertx, graphQlHandler, graphiQlHandler))
+    vertx.createHttpServer(HttpServerOptions().addWebSocketSubProtocol("graphql-ws"))
+      .requestHandler(applicationRouter(vertx, graphQlHandler, graphiQlHandler, wsHandler))
       .listen(8080)
   }
 }
--- src/main/kotlin/dev/marcinkiewicz/graphql/tasks/adapters/api/routers.kt
+++ src/main/kotlin/dev/marcinkiewicz/graphql/tasks/adapters/api/routers.kt
@@ -3,25 +3,18 @@ package dev.marcinkiewicz.graphql.tasks.adapters.api
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.graphql.ApolloWSHandler
 import io.vertx.ext.web.handler.graphql.GraphQLHandler
 import io.vertx.ext.web.handler.graphql.GraphiQLHandler
 
 internal fun applicationRouter(
   vertx: Vertx,
   graphQlHandler: GraphQLHandler,
-  graphiQlHandler: GraphiQLHandler
+  graphiQlHandler: GraphiQLHandler,
+  wsHandler: ApolloWSHandler
 ): Router = Router.router(vertx).apply {
-  mountSubRouter("/", graphQlRouter(vertx, graphQlHandler))
-  mountSubRouter("/", graphiQlRouter(vertx, graphiQlHandler))
+  route().handler(BodyHandler.create())
+  route("/graphql").handler(graphQlHandler)
+  route("/graphiql/*").handler(graphiQlHandler)
+  route("/graphql-ws").handler(wsHandler)
 }
-
-private fun graphQlRouter(vertx: Vertx, graphQlHandler: GraphQLHandler): Router =
-  Router.router(vertx).apply {
-    route().handler(BodyHandler.create())
-    route("/graphql").handler(graphQlHandler)
-  }
-
-private fun graphiQlRouter(vertx: Vertx, graphiQLHandler: GraphiQLHandler): Router =
-  Router.router(vertx).apply {
-    route("/graphiql/*").handler(graphiQLHandler)
-  }
